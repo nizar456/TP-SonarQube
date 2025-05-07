883a0f8bfdc001d0556ae037f27bfca4799b7c9c
l9irche <153326705+l9irche@users.noreply.github.com>
2025-05-07 16:35:57 +0100

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,5 +3,23 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is even");
+            } else {
+                System.out.println(i + " is odd");
+            }
+        }
+
+        duplicateCode();
+        duplicateCode();
+    }
+
+    public static void duplicateCode() {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
     }
 }
